2f38ba906b187931858f8991af6f76a9f87eff1b
Christy Lam <cyyllam@gmail.com>
2022-02-14 15:28:33 -0800

    convert pythons script to R

---
+++ process/OFM/download_ofm_apr.R
@@ -0,0 +1,37 @@
+library(here)
+
+download_ofm_apr1_est <- function(dataset, ...) {
+  root_url <-  'https://ofm.wa.gov/sites/default/files/public/dataresearch/pop/april1'
+  data_dir <-  'data'
+  kwargs <- list(...)
+  
+  if(dataset == 'post') {
+    if('posttype' %in% names(kwargs)){
+      if(kwargs['posttype'] == 'population') {
+        filename <- 'ofm_april1_population_final.xlsx'
+        dls <-  file.path(root_url, filename)
+        dest <-  here(data_dir, filename)
+      } else if (kwargs['posttype'] == 'housing') {
+        filename <- 'ofm_april1_housing.xlsx'
+        dls <-  file.path(root_url, filename)
+        dest <-  here(data_dir, filename)
+      } else {
+        stop("You're missing a posttype keyword argument. Indicate whether posttype is population or housing.\n")
+      }
+    }
+  } else if(dataset == 'inter') {
+    if(('start' %in% names(kwargs)) && ('end' %in% names(kwargs))) {
+      filename <- paste0('ofm_april1_intercensal_estimates_', kwargs['start'], '-', kwargs['end'], '.xlsx')
+      dls <-  file.path(root_url, 'hseries', filename)
+      dest <-  here(data_dir, filename)
+    } else {
+      stop("You're missing either start or end (or both) keyword arguments.\n")
+
+    }
+  } else {
+    stop("Use either 'post' or 'inter' for dataset argument\n")
+  }
+  download.file(dls, dest, mode = 'wb')
+}
+
+download_ofm_apr1_est('inter', start = '2000', end = '2010')
