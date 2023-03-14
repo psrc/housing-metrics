2e278cf2c5584b47fabc8792e199f92b36e1c372
Eric Clute <eclute@psrc.org>
2023-03-14 16:32:11 -0700

    Update renter_cost_burden_by_race.R

--- process/PUMS/renter_cost_burden_by_race.R
+++ process/PUMS/renter_cost_burden_by_race.R
@@ -9,9 +9,9 @@ library(psrccensus)
 library(dplyr)
 library(srvyr)
 
-x <- get_psrc_pums(5,2021,"h",c("PRACE","TEN","GRPIP","HINCP"))
-df <- x[[7]]
-df <- df %>% filter(TEN=="Rented") %>%
+rcb_raw <- get_psrc_pums(5,2021,"h",c("PRACE","TEN","GRPIP","HINCP"))
+rcb <- rcb_raw[[7]]
+rcb <- rcb %>% filter(TEN=="Rented") %>%
        mutate(
           income_bin=factor(case_when(HINCP < 25000 ~ "Under $25,000",
                                       HINCP < 35000 ~ "$25,000-$34,999",
@@ -34,4 +34,4 @@ df <- df %>% filter(TEN=="Rented") %>%
                                 levels=c("Greater than 50 percent",
                                          "Between 30 and 50 percent",
                                          "Less than 30 percent", "Else")))
-inc_rb_rgn <- psrc_pums_count(df, group_vars=c("PRACE","income_bin","rent_burden"))
+inc_rb_rgn <- psrc_pums_count(rcb, group_vars=c("PRACE","income_bin","rent_burden"))
