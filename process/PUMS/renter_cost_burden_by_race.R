f2f1fc7b5936309fa24e3ef526ecd5201498968b
Eric Clute <eclute@psrc.org>
2023-03-14 14:54:32 -0700

    Pushing existing versions to this repo

---
+++ process/PUMS/renter_cost_burden_by_race.R
@@ -0,0 +1,36 @@
+# TITLE: Renter Cost Burden by Race
+# GEOGRAPHIES: PSRC Region
+# DATA SOURCE: ACS PUMS
+# DATE MODIFIED: 3.14.2023
+# AUTHOR: Eric Clute
+
+library(magrittr)
+library(psrccensus)
+library(dplyr)
+library(srvyr)
+
+x <- get_psrc_pums(5,2021,"h",c("PRACE","TEN","GRPIP","HINCP"))
+x %<>% filter(TEN=="Rented") %>%
+       mutate(
+          income_bin=factor(case_when(HINCP < 25000 ~ "Under $25,000",
+                                      HINCP < 35000 ~ "$25,000-$34,999",
+                                      HINCP < 50000 ~ "$35,000-$49,999",
+                                      HINCP < 75000 ~ "$50,000-$74,999",
+                                      HINCP < 100000 ~ "$75,000-$99,999",
+                                      HINCP >=100000 ~ "$100,000 or more",
+                                      !is.na(HINCP) ~ "Else / Prefer not to answer"),
+                            levels=c("Under $25,000",
+                                     "$25,000-$34,999",                                     
+                                     "$35,000-$49,999",
+                                     "$50,000-$74,999",
+                                     "$75,000-$99,999",
+                                     "$100,000 or more",
+                                     "Else / Prefer not to answer")),
+            rent_burden=factor(case_when(GRPIP < 30 ~"Less than 30 percent",
+                                         between(GRPIP,30,50) ~ "Between 30 and 50 percent",
+                                         GRPIP > 50 ~ "Greater than 50 percent",
+                                         !is.na(GRPIP) ~ "Else"),
+                                levels=c("Greater than 50 percent",
+                                         "Between 30 and 50 percent",
+                                         "Less than 30 percent", "Else")))
+inc_rb_rgn <- psrc_pums_count(x, group_vars=c("PRACE","income_bin","rent_burden"))
---
+++ process/Redfin/Home_price_by_type.R
@@ -0,0 +1,64 @@
+# TITLE: Median Home Price by Type
+# GEOGRAPHIES: King and Snohomish Only (limited by Redfin)
+# DATA SOURCE: Redfin
+# DATE MODIFIED: 3.13.2023
+# AUTHOR: Eric Clute
+
+library(tidyverse)
+
+redfin_all <- read_tsv("https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_tracker/redfin_metro_market_tracker.tsv000.gz")
+
+# Seattle Metro area only includes King and Snohomish Counties. Tacoma = Pierce, and Bremerton = Kitsap
+seattle <- redfin_all %>%
+  filter(region == "Seattle, WA metro area") %>%
+  transmute(
+    date = period_begin,
+    region = region,
+    property_type = property_type,
+    median_sale_price = median_sale_price
+    )
+
+# tacoma <- redfin_all %>%
+#   filter(region == "Tacoma, WA metro area") %>%
+#   transmute(
+#     date = period_begin,
+#     region = region,
+#     property_type = property_type,
+#     median_sale_price = median_sale_price
+#   )
+# 
+# bremerton <- redfin_all %>%
+#   filter(region == "Bremerton, WA metro area") %>%
+#   transmute(
+#     date = period_begin,
+#     region = region,
+#     property_type = property_type,
+#     median_sale_price = median_sale_price
+#   )
+
+# Remove Multi-Family rows, sort by date & property_type
+
+seattle <- seattle %>%
+  filter(!(property_type == "Multi-Family (2-4 Unit)"))
+
+seattle <- seattle[order( seattle[,1], seattle[,3] ),]
+
+# Delete every 2nd row starting from 1 - keeps only the first row for each property_type (it's nicely rounded by Redfin)
+
+seattle <- seattle %>%filter(!(row_number() %% 2 != 1))
+
+# Sort by date and property type/pivot wide before exporting
+
+seattle <- seattle[order(as.Date(seattle$date),(factor(seattle$property_type, levels = c("All Residential", "Single Family Residential", "Townhouse", "Condo/Co-op")))),]
+
+seattle <- seattle %>%
+  pivot_wider(names_from = date , values_from = median_sale_price)
+
+# Exporting just Seattle Metro Data (King and Snohomish)
+
+library(openxlsx)
+
+work_book <- createWorkbook()
+addWorksheet(work_book, sheetName = "King & Snohomish")
+writeData(work_book, "King & Snohomish", seattle)
+saveWorkbook(work_book, file = "Median Home Price - Redfin/PriceByType-KingSnohomish.xlsx", overwrite = TRUE)
