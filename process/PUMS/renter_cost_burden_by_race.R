f3a2b12a0fae20380103561a5b8069804f60cbd0
Eric Clute <eclute@psrc.org>
2023-03-15 17:26:35 -0700

    Update renter_cost_burden_by_race.R

--- process/PUMS/renter_cost_burden_by_race.R
+++ process/PUMS/renter_cost_burden_by_race.R
@@ -9,8 +9,13 @@ library(psrccensus)
 library(dplyr)
 library(srvyr)
 
+# Pull PUMS data
+setwd("C:/Users/eclute/Downloads")
 rcb_raw <- get_psrc_pums(5,2021,"h",c("PRACE","TEN","GRPIP","HINCP"))
+setwd("J:/Projects/V2050/Housing/Monitoring/2023Update")
 rcb <- rcb_raw
+
+# Filter to only renters, create income/rent burden groupings
 rcb <- rcb %>% filter(TEN=="Rented") %>%
        mutate(
           income_bin=factor(case_when(HINCP < 25000 ~ "Under $25,000",
@@ -30,8 +35,20 @@ rcb <- rcb %>% filter(TEN=="Rented") %>%
             rent_burden=factor(case_when(GRPIP < 30 ~"Less than 30 percent",
                                          between(GRPIP,30,50) ~ "Between 30 and 50 percent",
                                          GRPIP > 50 ~ "Greater than 50 percent",
-                                         !is.na(GRPIP) ~ "Else"),
+                                         !is.na(GRPIP) ~ "No Rent Charged"),
                                 levels=c("Greater than 50 percent",
                                          "Between 30 and 50 percent",
-                                         "Less than 30 percent", "Else")))
+                                         "Less than 30 percent", "No Rent Charged")))
+
+# Create output table
 inc_rb_rgn <- psrc_pums_count(rcb, group_vars=c("PRACE","income_bin","rent_burden"))
+
+# Exporting table
+
+library(openxlsx)
+
+work_book <- createWorkbook()
+addWorksheet(work_book, sheetName = "RenterCostBurdenbyRE")
+writeData(work_book, "RenterCostBurdenbyRE", inc_rb_rgn)
+saveWorkbook(work_book, file = "Renter Cost Burden by RE\renter_cost_burden_by_RE.xlsx", overwrite = TRUE)
+
