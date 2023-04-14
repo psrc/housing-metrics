e3a7da8e730db004200dc4070feb864193d772a0
Eric Clute <eclute@psrc.org>
2023-04-14 11:44:05 -0700

    cleaned

--- process/Redfin/Home_price_by_type.R
+++ process/Redfin/Home_price_by_type.R
@@ -1,7 +1,7 @@
 # TITLE: Median Home Price by Type
 # GEOGRAPHIES: King and Snohomish Only (limited by Redfin)
 # DATA SOURCE: Redfin
-# DATE MODIFIED: 3.13.2023
+# DATE MODIFIED: 4.14.2023
 # AUTHOR: Eric Clute
 
 library(tidyverse)
@@ -18,44 +18,22 @@ seattle <- redfin_all %>%
     median_sale_price = median_sale_price
     )
 
-# tacoma <- redfin_all %>%
-#   filter(region == "Tacoma, WA metro area") %>%
-#   transmute(
-#     date = period_begin,
-#     region = region,
-#     property_type = property_type,
-#     median_sale_price = median_sale_price
-#   )
-# 
-# bremerton <- redfin_all %>%
-#   filter(region == "Bremerton, WA metro area") %>%
-#   transmute(
-#     date = period_begin,
-#     region = region,
-#     property_type = property_type,
-#     median_sale_price = median_sale_price
-#   )
-
 # Remove Multi-Family rows, sort by date & property_type
-
 seattle <- seattle %>%
   filter(!(property_type == "Multi-Family (2-4 Unit)"))
 
 seattle <- seattle[order( seattle[,1], seattle[,3] ),]
 
 # Delete every 2nd row starting from 1 - keeps only the first row for each property_type (it's nicely rounded by Redfin)
-
 seattle <- seattle %>%filter(!(row_number() %% 2 != 1))
 
 # Sort by date and property type/pivot wide before exporting
-
 seattle <- seattle[order(as.Date(seattle$date),(factor(seattle$property_type, levels = c("All Residential", "Single Family Residential", "Townhouse", "Condo/Co-op")))),]
 
 seattle <- seattle %>%
   pivot_wider(names_from = date , values_from = median_sale_price)
 
 # Exporting just Seattle Metro Data (King and Snohomish)
-
 library(openxlsx)
 
 work_book <- createWorkbook()
