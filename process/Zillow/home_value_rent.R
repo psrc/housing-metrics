4af683154dde37cba6b92c7031688ceaab8d8cea
Eric Clute <eclute@psrc.org>
2023-08-18 14:33:45 -0700

    finalized zillow script

--- process/Zillow/home_value_rent.R
+++ process/Zillow/home_value_rent.R
@@ -1,20 +1,19 @@
 # TITLE: Home Value & Rent 
 # GEOGRAPHIES: Seattle MSA
 # DATA SOURCE: Zillow
-# DATE MODIFIED: 8.11.2023
+# DATE MODIFIED: 8.18.2023
 # AUTHOR: Eric Clute
 
 library(openxlsx)
 library(tidyr)
 library(stringr)
 library(dplyr)
+library(magrittr)
 
 # assumptions
 #  ZHVI: Zillow Home Value Index - All Homes (SFR & Condo) Time series, Smoothed, Seasonally-Adjusted
 #  ZORI: Zillow Observed Rent Index - All Homes + Multifamily, Smoothed, Seasonally-Adjusted
 
-start_date <- "2015/01/06"
-end_date <- "2023/30/06"
 ZHVI_url <- "https://files.zillowstatic.com/research/public_csvs/zhvi/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv"
 ZORI_url <- "https://files.zillowstatic.com/research/public_csvs/zori/Metro_zori_sm_sa_month.csv?t=1691785225"
 
@@ -29,13 +28,10 @@ ZORI <- subset(ZORI_raw, ZORI_raw$RegionName == 'Seattle, WA')
 ZHVI$source <- "ZHVI"
 ZORI$source <- "ZORI"
 
-# ZHVI <- subset(ZHVI, select = c(RegionName,X2015.01.31))
-# 
-# all_data <- rbind(ZHVI,ZORI)
-# 
-# 
-# 
-# ifelse()
-# names(ZHVI)=str_sub(names(ZHVI),2)
-# names(ZORI)=str_sub(names(ZORI),2)
+common <- intersect(colnames(ZHVI), colnames(ZORI))
+all_data <- rbind(ZHVI[common], ZORI[common])
 
+all_data %<>%
+  relocate(source, .before = RegionID)
+
+colnames(all_data)<-gsub("X","",colnames(all_data))
