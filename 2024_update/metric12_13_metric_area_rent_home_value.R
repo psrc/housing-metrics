eb30791ea775b3dcac648cb593ee48ed2d86ae5f
Eric Clute <eclute@psrc.org>
2024-06-24 10:44:04 -0600

    pulled data

--- 2024_update/metric12_13_metric_area_rent_home_value.R
+++ 2024_update/metric12_13_metric_area_rent_home_value.R
@@ -1,7 +1,7 @@
 # TITLE: Home Value & Rent 
 # GEOGRAPHIES: Seattle MSA
 # DATA SOURCE: Zillow
-# DATE MODIFIED: 3.28.2024
+# DATE MODIFIED: 6.24.2024
 # AUTHOR: Eric Clute
 
 library(openxlsx)
@@ -14,7 +14,7 @@ library(magrittr)
 #  ZHVI: Zillow Home Value Index - All Homes (SFR & Condo) Time series, Smoothed, Seasonally-Adjusted
 #  ZORI: Zillow Observed Rent Index - All Homes + Multifamily, Smoothed, Seasonally-Adjusted
 
-export_path <- "J:/Projects/V2050/Housing/Monitoring/2024Update/Data/metric12_metro_area_rent"
+export_path <- "J:/Projects/V2050/Housing/Monitoring/2024Update/Data/metric12-13_metro_area_rent_home_value"
 
 ZHVI_url <- "https://files.zillowstatic.com/research/public_csvs/zhvi/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv"
 ZORI_url <- "https://files.zillowstatic.com/research/public_csvs/zori/Metro_zori_uc_sfrcondomfr_sm_sa_month.csv?t=1711667054"
@@ -39,4 +39,4 @@ all_data %<>%
 colnames(all_data)<-gsub("X","",colnames(all_data))
 
 # Export
-#write.csv(all_data, file = file.path(export_path, "metric12_13_raw.csv"), row.names = FALSE)
+write.csv(all_data, file = file.path(export_path, "metric12_13_raw.csv"), row.names = FALSE)
